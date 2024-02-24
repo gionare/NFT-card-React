18413061e6ffb2f53334549ac2566fc745b2424f
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-02-23 19:55:40 -0500

    add few container styles

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,28 +6,34 @@ import Avatar from "/images/image-avatar.png";
 
 export default function Card() {
   return (
-    <div className="container">
-      <img className="equilibrium" src={Equilibrium} alt="Equilibrium image" />
-      <h1 className="title"> Equilibrium #3429</h1>
-      <p className="text">
-        {" "}
-        Our Equilibrium collection promotes balance and calm.{" "}
-      </p>
-      <div className="info-container">
-        <div className="eth">
-          <img src={Etherium} alt="Etherium image" />
-          <span>0.041 ETH</span>
-        </div>
-        <div className="clock">
-          <img src={Clock} alt="clock image" />
-          <span>3 days left</span>
-        </div>
-      </div>
-      <div className="avatar">
-        <img src={Avatar} alt="Avatar image" />
-        <p>
-          Create of <span>Jules Wyvern</span>
+    <div className="outfit">
+      <div className="container">
+        <img
+          className="equilibrium"
+          src={Equilibrium}
+          alt="Equilibrium image"
+        />
+        <h1 className="title"> Equilibrium #3429</h1>
+        <p className="text">
+          {" "}
+          Our Equilibrium collection promotes balance and calm.{" "}
         </p>
+        <div className="info-container">
+          <div className="eth">
+            <img src={Etherium} alt="Etherium image" />
+            <span>0.041 ETH</span>
+          </div>
+          <div className="clock">
+            <img src={Clock} alt="clock image" />
+            <span>3 days left</span>
+          </div>
+        </div>
+        <div className="avatar">
+          <img src={Avatar} alt="Avatar image" />
+          <p>
+            Create of <span>Jules Wyvern</span>
+          </p>
+        </div>
       </div>
     </div>
   );
